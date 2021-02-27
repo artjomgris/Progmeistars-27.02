4b75dd621bef1bded0d52f2aa1fa82db749c1a6e
agrisajevs <agrisajevs@edu.riga.lv>
2021-02-27 12:04:53 +0200

    Add time

--- client/client.go
+++ client/client.go
@@ -5,6 +5,7 @@ import (
 	"encoding/binary"
 	"fmt"
 	"net"
+	"time"
 )
 
 func main() {
@@ -15,11 +16,14 @@ func main() {
 	}
 
 	var data struct {
-		L   float64
-		Cnt int32
+		X    int32
+		Y    int32
+		Time []byte
 	}
-	data.L = 325.54
-	data.Cnt = 34
+
+	data.X = 1
+	data.Y = 2
+	data.Time = []byte(time.Now().String())
 
 	var buf bytes.Buffer
 	err = binary.Write(&buf, binary.LittleEndian, data)
--- server/server.go
+++ server/server.go
@@ -5,7 +5,10 @@ import (
 	"encoding/binary"
 	"fmt"
 	"net"
+	"time"
 )
+
+var layout = "2006-01-02T15:04:05.000Z"
 
 func main() {
 	adr, err := net.ResolveUDPAddr("udp", "localhost:5000")
@@ -27,7 +30,7 @@ func main() {
 }
 
 func handleConnection(con *net.UDPConn) {
-	buf := make([]byte, 2000)
+	buf := make([]byte, 3000)
 	n, err := con.Read(buf)
 	if err != nil {
 		fmt.Println(err)
@@ -36,14 +39,21 @@ func handleConnection(con *net.UDPConn) {
 	buff := bytes.NewReader(buf[0:n])
 
 	var data struct {
-		L   float64
-		Cnt int32
+		X    int32
+		Y    int32
+		Time []byte
 	}
+
 	err = binary.Read(buff, binary.LittleEndian, &data)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println(data)
+	t, err := time.Parse(layout, string(data.Time))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(data, t)
 }
